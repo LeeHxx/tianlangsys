45e410921dbbd22b04ce4e5dd9d00f755b5e4936
LeeHxx <spongebob7037@hotmail.com>
2019-12-30 23:11:30 +0800

    script模块

---
+++ script.php
@@ -0,0 +1,12 @@
+<script src="scripts/jquery-3.3.1.min.js"></script>
+<script src="scripts/popper.min.js"></script>
+<script src="scripts/bootstrap.js"></script>
+<script src="scripts/Chart.min.js"></script>
+<script src="scripts/shards.js"></script>
+<script src="scripts/buttons.js"></script>
+<script src="scripts/jquery.sharrre.min.js"></script>
+<script src="scripts/extras.1.3.1.min.js"></script>
+<script src="scripts/shards-dashboards.1.3.1.js"></script>
+<script src="scripts/jquery.dataTables.js"></script>
+<script src="scripts/dataTables.responsive.js"></script>
+<script src="scripts/app/app-transaction-history.1.3.1.min.js"></script>
