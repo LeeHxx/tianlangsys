011fdfe5f06ca38df665ba6ae5b0e50a0bfca6b0
LeeHxx <spongebob7037@hotmail.com>
2020-01-15 11:01:51 +0800

    简化head模块

--- head.php
+++ head.php
@@ -3,36 +3,22 @@
 <title>生产计划进度管理系统</title>
 <meta name="description" content="tianlang">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="Shortcut Icon" href="images/TLlogo-1.png" />
+<link rel="bookmark" href="images/TLlogo-1.png" />
 <link rel="stylesheet" href="styles/all.css">
 <link rel="stylesheet" href="styles/icon.css">
 <link rel="stylesheet" href="styles/bootstrap.css">
-<link rel="stylesheet" href="styles/shards-dashboards.1.3.1.css" id="main-stylesheet" data-version="1.3.1">
+<link rel="stylesheet" href="styles/shards-dashboards.1.3.1.css?v=20200115" id="main-stylesheet" data-version="1.3.1">
 <link rel="stylesheet" href="styles/extras.1.3.1.min.css">
 <link rel="stylesheet" href="styles/responsive.dataTables.css">
 <link rel="stylesheet" href="styles/font-awesome.css">
 <script src="scripts/jquery-3.3.1.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function(){
-
-    //$(".aaa").find("a").each(function () {
-    $(".aaa a").each(function () {
-    //$(".aaa a").click(function () {
-      //$(".aaa a").removeClass("active");
-      //$(this).addClass("active");
-      //alert(document.location.href.search(this.href));
-      //alert(this.href);
-      if (location.href.search(this.href) >= 0) {
-                $(this).addClass('active'); // this.className = 'active';
-            }
-      //if (this.href == location.pathname || document.location.href.search(this.href) >= 0) {
-      //          $(this).parent().addClass('active'); // this.className = 'active';
-      //      }
-      //if ($(a).attr("href") === location.pathname) {
-      //  $(this).addClass("active");
-      //} else {
-      //  $(this).removeClass("active");
-      //}
-    });
-
+  $(".aaa a").each(function () {
+    if (location.href.search(this.href) >= 0) {
+      $(this).addClass('active');
+    }
+  });
 });
 </script>
