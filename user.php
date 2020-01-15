5f5f1ff602a413a7524076d7a76a1728ef85c525
LeeHxx <spongebob7037@hotmail.com>
2020-01-15 11:05:50 +0800

    更新用户管理

--- user.php
+++ user.php
@@ -4,19 +4,7 @@ session_start();?>
 <!DOCTYPE html>
 <html class="no-js h-100" lang="zh-CN">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>生产计划进度管理系统</title>
-  <meta name="description" content="tianlang">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="styles/all.css">
-  <link rel="stylesheet" href="styles/icon.css">
-  <link rel="stylesheet" href="styles/bootstrap.min.css">
-  <link rel="stylesheet" id="main-stylesheet" data-version="1.3.1" href="styles/shards-dashboards.1.3.1.css">
-  <link rel="stylesheet" href="styles/extras.1.3.1.min.css">
-  <link rel="stylesheet" href="styles/responsive.dataTables.css">
-  <link rel="stylesheet" href="styles/font-awesome.css">
-  <script src="scripts/jquery-3.3.1.min.js"></script>
+  <?php include('head.php') ?>
   <script type="text/javascript">
     function usable (id) {
       $.post("usable.php?id="+id,function(data){
@@ -37,153 +25,12 @@ session_start();?>
   <div class="container-fluid">
     <div class="row">
       <!-- 侧栏 -->
-      <aside class="main-sidebar col-12 col-md-3 col-lg-2 px-0">
-        <div class="main-navbar">
-          <nav class="navbar align-items-stretch navbar-light bg-white flex-md-nowrap border-bottom p-0">
-            <a class="navbar-brand w-100 mr-0" href="#" style="line-height: 25px;">
-              <div class="d-table m-auto">
-                <!--<img id="main-logo" class="d-inline-block align-top mr-1" style="max-width: 25px;" src="images/logo.svg" alt="Shards Dashboard">-->
-                <span class="  d-md-inline ml-1">生产管理系统</span>
-              </div>
-            </a>
-            <a class="toggle-sidebar d-sm-inline d-md-none d-lg-none">
-              <i class="material-icons">&#xE5C4;</i>
-            </a>
-          </nav>
-        </div>
-
-        <div class="nav-wrapper">
-          <ul class="nav nav--no-borders flex-column">
-            <li class="nav-item dropdown open">
-              <a class="nav-link dropdown-toggle  " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                <i class="material-icons">insert_drive_file</i>
-                <span>订单管理</span>
-              </a>
-              <div class="dropdown-menu dropdown-menu-small">
-                <a class="dropdown-item  " href="order.php">订单列表</a>
-                <a class="dropdown-item " href="order-add.php">添加订单</a>
-              </div>
-            </li>
-            <li class="nav-item  dropdown">
-              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="true">
-                <i class="material-icons">assignment_returned</i>
-                <span>物料管理</span>
-              </a>
-              <div class="dropdown-menu dropdown-menu-small">
-                <a class="dropdown-item " href="material.php">物料列表</a>
-                <a class="dropdown-item " href="material-search.php">添加</a>
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                <i class="material-icons">straighten</i>
-                <span>工艺管理</span>
-              </a>
-              <div class="dropdown-menu dropdown-menu-small">
-                <a class="dropdown-item " href="process.php">工艺列表</a>
-                <a class="dropdown-item " href="process-add.php">添加</a>
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="true">
-                <i class="material-icons">home_work</i>
-                <span>仓库管理</span>
-              </a>
-              <div class="dropdown-menu dropdown-menu-small">
-                <a class="dropdown-item " href="warehouse.php">仓库列表</a>
-                <a class="dropdown-item " href="warehouse-add.php">添加</a>
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="true">
-                <i class="material-icons">all_inclusive</i>
-                <span>生产制造管理</span>
-              </a>
-              <div class="dropdown-menu dropdown-menu-small">
-                <a class="dropdown-item " href="workshop-smt.php">车间作业</a>
-                <a class="dropdown-item " href="problem.php">问题反馈记录单</a>
-                <a class="dropdown-item " href="addition.php">物料补领申请单</a>
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="true">
-                <i class="material-icons">playlist_add_check</i>
-                <span>质量管理</span>
-              </a>
-              <div class="dropdown-menu dropdown-menu-small">
-                <a class="dropdown-item " href="quality.php">质量列表</a>
-                <a class="dropdown-item " href="quality-add.php">添加</a>
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="true">
-                <i class="material-icons">local_shipping</i>
-                <span>成品管理</span>
-              </a>
-              <div class="dropdown-menu dropdown-menu-small">
-                <a class="dropdown-item " href="product.php">成品列表</a>
-                <a class="dropdown-item " href="product-add.php">添加成品</a>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </aside>
+      <?php include('sidebar.php') ?>
       <!-- End 侧栏 -->
       <main class="main-content col-lg-10 col-md-9 col-sm-12 p-0 offset-lg-2 offset-md-3">
         <div class="main-navbar sticky-top bg-white">
           <!-- 顶栏 -->
-          <nav class="navbar align-items-stretch navbar-light flex-md-nowrap p-0">
-            <div  class=" w-100 d-none d-md-flex d-lg-flex">
-              <div class="d-table " style="margin-top: auto !important;margin-right: auto !important;margin-bottom: auto !important;margin-left: auto !important;">
-                <img id="main-logo" class="d-inline-block align-top mr-1" style="max-height: 33px;" src="images/TLlogo-1.png" alt="Dashboard">
-                <a class="d-md-inline ml-1" style="font-size: 22px; color: #3d5170; font-weight: 900;">&nbsp&nbsp&nbsp南京天朗电子装备有限公司</a>
-              </div>
-            </div>
-            <ul class="navbar-nav border-left flex-row ">
-              <li class="nav-item border-right dropdown notifications">
-                <a class="nav-link nav-link-icon text-center" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <div class="nav-link-icon__wrapper">
-                    <i class="material-icons">&#xE7F4;</i>
-                    <span class="badge badge-pill badge-danger">2</span>
-                  </div>
-                </a>
-                <div class="dropdown-menu dropdown-menu-small" aria-labelledby="dropdownMenuLink">
-                  <a class="dropdown-item" href="#">
-                    <div class="notification__icon-wrapper">
-                      <div class="notification__icon">
-                        <i class="material-icons">&#xE6E1;</i>
-                      </div>
-                    </div>
-                    <div class="notification__content">
-                      <span class="notification__category">消息</span>
-                      <p>仓库齐套来料排配超时！<span class="text-success text-semibold"></span> 请尽快处理！</p>
-                    </div>
-                  </a>
-
-                  <a class="dropdown-item notification__all text-center" href="#"> 查看所有消息 </a>
-                </div>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-nowrap px-3" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                  <img class="user-avatar rounded-circle mr-2" src="images/avatars/admin.png" alt="User Avatar"> <span class="d-none d-md-inline-block"><?php echo $_SESSION['user'];?></span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-small">
-                  <a class="dropdown-item" href="userInfo.php"><i class="material-icons">&#xE7FD;</i> 账户信息</a>
-
-                  <?php if($_SESSION['admin']=='1'){?>
-                    <a class="dropdown-item" href="user.php"><i class="material-icons">&#xE8B8;</i> 账户管理</a>
-                  <?php }?>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item text-danger" href="login.php"><i class="material-icons text-danger">&#xE879;</i> 安全退出 </a>
-                </div>
-              </li>
-            </ul>
-            <nav class="nav">
-              <a href="#" class="nav-link nav-link-icon toggle-sidebar d-sm-inline d-md-none text-center border-left" data-toggle="collapse" data-target=".header-navbar" aria-expanded="false" aria-controls="header-navbar">
-                <i class="material-icons">&#xE5D2;</i>
-              </a>
-            </nav>
-          </nav>
+          <?php include('navbar.php') ?>
         </div>
         <!-- Page -->
         <div class="main-content-container container-fluid px-4 mb-4">
@@ -335,16 +182,6 @@ session_start();?>
       </main>
     </div>
   </div>
-  <script src="scripts/jquery-3.3.1.min.js"></script>
-  <script src="scripts/popper.min.js"></script>
-  <script src="scripts/bootstrap.js"></script>
-  <script src="scripts/Chart.min.js"></script>
-  <script src="scripts/shards.js"></script>
-  <script src="scripts/buttons.js"></script>
-  <script src="scripts/jquery.sharrre.min.js"></script>
-  <script src="scripts/extras.1.3.1.min.js"></script>
-  <script src="scripts/shards-dashboards.1.3.1.js"></script>
-  <script src="scripts/jquery.dataTables.js"></script>
-  <script src="scripts/dataTables.responsive.js"></script>
+  <?php include('script.php') ?>
 </body>
 </html>
