846e046074c3ed318b5cc592143944302ac0fd20
LeeHxx <spongebob7037@hotmail.com>
2020-01-15 11:05:02 +0800

    修改侧边栏

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar col-12 col-md-3 col-lg-2 px-0">
   <div class="main-navbar">
     <nav class="navbar align-items-stretch navbar-light bg-white flex-md-nowrap border-bottom p-0">
-      <a class="navbar-brand w-100 mr-0" href="index.php" style="line-height: 25px;">
+      <a class="navbar-brand w-100 mr-0" href="welcome.php" style="line-height: 25px;">
         <div class="d-table m-auto">
           <!--<img id="main-logo" class="d-inline-block align-top mr-1" style="max-width: 25px;" src="images/logo.svg" alt="Shards Dashboard">-->
           <span class="  d-md-inline ml-1">生产管理系统</span>
@@ -61,7 +61,7 @@
           <span>生产制造管理</span>
         </a>
         <div class="dropdown-menu dropdown-menu-small">
-          <div class="dropdown">
+          <div class="dropdown bbb">
           <a class="dropdown-item  " data-toggle="dropdown" href="workshop">车间作业</a>
           <div class="dropdown-menu  dropdown-menu-small">
             <a class="dropdown-item " href="workshop-smt.php">SMT</a>
