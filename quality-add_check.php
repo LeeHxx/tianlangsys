04701ad54df081e3913a416376e643c78b3119ba
cjq777 <59111046+cjq777@users.noreply.github.com>
2020-02-24 13:34:38 +0800

    添加质量情况后端

---
+++ quality-add_check.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+header("Content-type:text/html; charset=utf-8");
+require_once('conn.php');
+$id=$_POST['order_id'];
+$first_date=$_POST['quality_first_date'];
+$first_inspection=$_POST['quality_first_inspection'];
+$first_confirm=$_POST['quality_first_confirm'];
+$batch_inspect=$_POST['quality_batch_inspect'];
+$inspection=$_POST['quality_inspection'];
+$OK_volume=$_POST['quality_OK_volume'];
+$NG_volume=$_POST['quality_NG_volume'];
+$inspection_confirm=$_POST['quality_inspection_confirm'];
+$inspection_date=$_POST['quality_inspection_date'];
+
+$sql="insert into quality(order_id,quality_first_date,quality_first_inspection,quality_first_confirm,quality_batch_inspect,quality_inspection,quality_OK_volume,quality_NG_volume,quality_inspection_confirm,quality_inspection_date) values ('$id','$first_date','$first_inspection','$first_confirm','$batch_inspect','$inspection','$OK_volume','$NG_volume','$inspection_confirm','$inspection_date')";
+
+$result=mysqli_query($conn,$sql);
+
+if($result){
+    header("Location: quality-add-success.php");
+    echo "yes";
+	exit;
+}else{
+  header("Location: quality-add.php");
+  echo "no";
+	die('Could not connect:' .mysqli_error());
+	exit;
+	}
+
+mysqli_close($conn);
+
+?>
