ada3d81eb9fb06979f120e5ee7af33a77b641fb4
LeeHxx <spongebob7037@hotmail.com>
2020-03-03 00:34:01 +0800

    订单添加失败界面修改

--- order-add-fail.php
+++ order-add-fail.php
@@ -69,74 +69,74 @@ session_start();?>
             </div>
           </div>
           <div class="row">
-            <div class="col-lg-8 mx-auto mt-4">
+            <div class="col-lg-12 mx-auto">
               <div class="card card-small mb-4">
                 <div class="card-body p-0">
                   <form action="order-add_check.php" class="py-4" id="order_add" method="post">
-                    <div class="form-row mx-4 ">
-                      <div class="col  ">
-                        <h5 class="form-text m-0">订单信息</h5>
-                      </div>
-                    </div>
-                    <hr>
-                    <div class="form-row mx-4">
-                      <div class="form-group col-md-6">
-                        <label for="order_id">订单号</label>
-                        <input type="text" class="form-control" name="order_id" id="order_id" value="" placeholder="订单号">
-                      </div>
-                      <div class="form-group col-md-6">
-                        <label for="order_name">品名</label>
-                        <input type="text" class="form-control" name="order_name" id="order_name" value="" placeholder="品名">
-                      </div>
-                    </div>
-                    <div class="form-row mx-4">
-                      <div class="form-group col-md-6">
-                        <label for="order_type">规格/型号/图号</label>
-                        <input type="text" class="form-control" name="order_type" id="order_type" value="" placeholder="规格/型号/图号">
-                      </div>
-                      <div class="form-group col-md-6">
-                        <label for="order_volume">订单量</label>
-                        <input type="number" class="form-control" name="order_volume" id="order_volume" value="" placeholder="订单量">
-                      </div>
-                    </div>
-                    <div class="form-row mx-4">
-                      <div class="form-group col-md-12">
-                        <label for="order_client">客户</label>
-                        <input type="text" class="form-control" name="order_client" id="order_client" value="" placeholder="客户">
-                      </div>
-                      <div class="form-group col-md-12">
-                        <label for="firstName">接交单日期</label>
-                        <div class="input-daterange input-group" id="transaction-history-date-range">
-                          <span class="input-group-prepend">
-                            <span class="input-group-text">
-                              <i class="fa fa-calendar"></i>
-                            </span>
-                          </span>
-                          <input type="text" class="input-sm form-control" name="order_start" id="order_start"  placeholder="接单日期" />
-                          <span class="input-group-middle"><span class="input-group-text">-</span></span>
-                          <input type="text" class="input-sm form-control" name="order_end" id="order_end" placeholder="交货日期" />
-                          <span class="input-group-append">
-                            <span class="input-group-text">
-                              <i class="fa fa-calendar"></i>
-                            </span>
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                  </form>
-                </div>
-                <div class="card-footer border-top">
-                  <div class="col">
-                    <button id="btn1" form="order_add" class="btn  btn-accent mx-auto d-table mr-3"><i class="fa fa-check mr-1"></i>添加订单</button>
+                    <!--<div class="form-row mx-4 ">
+                    <div class="col  ">
+                    <h5 class="form-text m-0">订单信息</h5>
                   </div>
                 </div>
+                <hr>-->
+                <div class="form-row mx-4 mt-2">
+                  <div class="form-group col-md-3">
+                    <label for="order_id">订单号</label>
+                    <input type="text" class="form-control" name="order_id" id="order_id" value="" placeholder="订单号">
+                  </div>
+                  <div class="form-group col-md-3">
+                    <label for="order_name">品名</label>
+                    <input type="text" class="form-control" name="order_name" id="order_name" value="" placeholder="品名">
+                  </div>
+
+                  <div class="form-group col-md-3">
+                    <label for="order_type">规格/型号/图号</label>
+                    <input type="text" class="form-control" name="order_type" id="order_type" value="" placeholder="规格/型号/图号">
+                  </div>
+                  <div class="form-group col-md-3">
+                    <label for="order_volume">订单量</label>
+                    <input type="number" class="form-control" name="order_volume" id="order_volume" value="" placeholder="订单量">
+                  </div>
+                </div>
+                <div class="form-row mx-4">
+                  <div class="form-group col-md-4">
+                    <label for="order_client">客户</label>
+                    <input type="text" class="form-control" name="order_client" id="order_client" value="" placeholder="客户名称">
+                  </div>
+                  <div class="form-group col-md-8">
+                    <label for="firstName">接交单日期</label>
+                    <div class="input-daterange input-group" id="transaction-history-date-range">
+                      <span class="input-group-prepend">
+                        <span class="input-group-text">
+                          <i class="fa fa-calendar"></i>
+                        </span>
+                      </span>
+                      <input type="text" class="input-sm form-control" name="order_start" id="order_start"  placeholder="接单日期" />
+                      <span class="input-group-middle"><span class="input-group-text">-</span></span>
+                      <input type="text" class="input-sm form-control" name="order_end" id="order_end" placeholder="交货日期" />
+                      <span class="input-group-append">
+                        <span class="input-group-text">
+                          <i class="fa fa-calendar"></i>
+                        </span>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+
+              </form>
+            </div>
+            <div class="card-footer border-top">
+              <div class="col">
+                <button id="btn1" form="order_add" class="btn  btn-accent mx-auto d-table mr-3"><i class="fa fa-check mr-1"></i>添加订单</button>
               </div>
             </div>
           </div>
         </div>
-      </main>
+      </div>
     </div>
-  </div>
-  <?php include('script.php') ?>
+  </main>
+</div>
+</div>
+<?php include('script.php') ?>
 </body>
 </html>
