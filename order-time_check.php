186ed87ccf2e3381b09e8c959fe5329eef24e217
LeeHxx <spongebob7037@hotmail.com>
2020-04-05 18:24:40 +0800

    订单提醒实验性开发

---
+++ order-time_check.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+header("Content-type:text/html; charset=utf-8");
+require_once('conn.php');
+
+$id = $_GET['id'];
+$sql="select * from orders where ";
+//执行sql语
+
+//mysqli_query($conn,$sql) or die('添加数据出错：'.mysql_error());
+$result=mysqli_query($conn,$sql);
+if($result){
+  echo "yes";
+	exit;
+}else{
+	echo "no";
+	die('Could not connect:' .mysqli_error());
+	exit;
+	}
+
+//关闭数据库连接
+mysqli_close($conn);
+?>
