630221fd00099774f7fa1271eb5c1091ce81a834
LeeHxx <spongebob7037@hotmail.com>
2020-04-18 02:52:17 +0800

    Update order-time_check.php

--- order-time_check.php
+++ order-time_check.php
@@ -2,22 +2,20 @@
 session_start();
 header("Content-type:text/html; charset=utf-8");
 require_once('conn.php');
-
-$id = $_GET['id'];
-$sql="select * from orders where ";
-//执行sql语
-
-//mysqli_query($conn,$sql) or die('添加数据出错：'.mysql_error());
+date_default_timezone_set('Asia/Shanghai');
+$time_now=strtotime(date("Y-m-d"));
+$tt = date("Y-m-d H:i:s");
+echo $tt;
+$sql="select * from orders where order_finished=0";
 $result=mysqli_query($conn,$sql);
-if($result){
-  echo "yes";
-	exit;
-}else{
-	echo "no";
-	die('Could not connect:' .mysqli_error());
-	exit;
-	}
-
-//关闭数据库连接
+$loginNum=mysqli_num_rows($result);
+for($i=0; $i<$loginNum; $i++){
+  $row = mysqli_fetch_assoc($result);
+  $time_end = strtotime($row['order_end']);
+  $time_left =ceil(($time_end-$time_now)/86400);
+  // echo $row['order_id'];
+  // echo $row['order_end'];
+  echo $time_left;
+}
 mysqli_close($conn);
 ?>
